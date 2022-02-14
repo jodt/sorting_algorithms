92918ecd06acea4ca3288180a04d3f0aa6e6dac0
Secatricia <3560@holbertonschool.com>
2022-02-14 14:12:40 +0000

    Add file 102-O and 102-counting_sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * counting_sort - function that sorts an array
+ * of integers in ascending order using the 
+ * Counting sort algorithm
+ * @array: array
+ * @size: array size
+ */
+void counting_sort(int *array, size_t size)
+{
+	
+}
