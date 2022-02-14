7165df248bffd6109031279fdf45677bcd48fa34
Secatricia <3560@holbertonschool.com>
2022-02-14 09:02:47 +0000

    Add ex 0 and  1

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+ * selection_sort - function that sorts
+ * an array of integers in ascending order
+ * using the Selection sort algorithm
+ *
+ * @array: array of unsorted int
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t j = 0;
+	size_t index;
+	size_t value;
+	size_t j_comp;
+	size_t comp_value;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		value = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				j_comp = array[j];
+				if (value > j_comp)
+				{
+					index = j;
+					value = array[j];
+				}
+			}
+		}
+		comp_value = array[i];
+
+		array[index] = array[i];
+		array[i] = value;
+		if (comp_value != value)
+		{
+			print_array(array, size);
+		}
+	}
+}
