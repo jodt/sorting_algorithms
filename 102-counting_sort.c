b795b337191cb2b68f0b0521755eae2f9ed421b5
Secatricia <3560@holbertonschool.com>
2022-02-15 10:01:52 +0000

    102 free

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,24 +10,24 @@ void counting_sort(int *array, size_t size)
 {
 	if (size > 1)
 	{
-		int *new, counter = 0, j = 0, i = 0, biggest_value = 0;
-		int n_size = size, k = 0;
+		int *new, counter = 0, j = 0, i = 0, k = 0;
+		int n_size = size, l = 0;
 
 		for (i = 0; i < n_size; i++)
 		{
-			if (array[i] > biggest_value)
+			if (array[i] > k)
 			{
-				biggest_value = array[i];
+				k = array[i];
 			}
 		}
-		biggest_value += 1;
-		new = malloc(sizeof(int) * biggest_value);
+		k += 1;
+		new = malloc(sizeof(int) * k);
 		if (new == NULL)
 		{
 			return;
 		}
 
-		for (i = 0; i < biggest_value; i++)
+		for (i = 0; i < k; i++)
 		{
 			for (j = 0; j < n_size; j++)
 			{
@@ -39,15 +39,16 @@ void counting_sort(int *array, size_t size)
 			new[i] = counter;
 		}
 		counter = 0;
-		for (i = 0; i < biggest_value; i++)
+		for (i = 0; i < k; i++)
 		{
 			if (new[i] > counter)
 			{
-				array[k] = i;
+				array[l] = i;
 				counter += 1;
-				k++;
+				l++;
 			}
 		}
-		print_array(new, biggest_value);
+		print_array(new, k);
+		free(new);
 	}
 }
